de73906deee37ac83efe7972007ae41f02c3cd42
TGG <phhaslbauer@yahoo.de>
2023-09-29 13:39:36 +0200

    clean up

--- setup.py
+++ setup.py
@@ -33,8 +33,9 @@ if __name__ == '__main__':
             'License :: OSI Approved :: Apache Software License',
             'Operating System :: OS Independent',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
         ],
         license='GPLv3',
         install_requires=get_requirements()
